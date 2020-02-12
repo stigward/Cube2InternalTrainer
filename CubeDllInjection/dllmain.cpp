71e18f51632ba16ccd15f3ddd4cf6d0c7cd98aae
jmagi98 <jmaginnes@gmail.com>
2020-02-12 00:13:27 -0500

    added maxAmmo hack that gives user all weapons with unlimited ammo upon startup

--- CubeDllInjection/dllmain.cpp
+++ CubeDllInjection/dllmain.cpp
@@ -17,20 +17,44 @@ DWORD WINAPI InjectionThread(HMODULE hmod)
 	uintptr_t modBase = (uintptr_t)GetModuleHandle(L"sauerbraten.exe");
 	
 	bool godMode = true;
+	bool maxAmmo = true;
 	std::cout << "In the injection thread\n";
 
 	while (true)
 	{
-			// get address
+		// get address
 		uintptr_t pointer = modBase + 0x29D2C0;
+		uintptr_t visual = modBase + 0x2074A4;
+		int healthVal = 9999;
 		if (godMode)
 		{
 			uintptr_t addr = FindDMAAddr(pointer, { 0x0, 0x340 });
 			// create a pointer to the address we just found
 			int* health = (int*)addr;
 			//deref that pointer and fill health back up	
-			*health = 9999;
+			*health = healthVal;
+
+			// same thing but for the UI. Otherwise it would look like we have 100 instead of 9999
+			uintptr_t v_addr = FindDMAAddr(visual, { 0x15c });
+			int *v_health = (int*)v_addr;
+			*v_health = healthVal;
 			Sleep(1);	
+		}
+
+		if (maxAmmo) {
+			uintptr_t ptr = modBase + 0x2074A4;
+			uintptr_t ammoAddr = FindDMAAddr(ptr , { 0x17c });
+			// total number of guns in the game
+			int numGuns = 6;
+			int ammoVal = 9999;
+			for (unsigned int i = 0; i <= numGuns; ++i)
+			{
+				uintptr_t currentAddr = ammoAddr + i*4;
+				int *ammo = (int*)currentAddr;
+				*ammo = ammoVal;
+				
+			}
+
 		}
 
 	}
